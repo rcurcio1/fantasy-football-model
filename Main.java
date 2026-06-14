a272742a928a2461893309459c50fc1d44f6640b
rcurcio1 <rosskcurcio@gmail.com>
2026-06-14 16:58:00 -0400

    Start creating the q-learning framework within the OGDraftStrategy

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayDeque;
 public static void main(String[] args) {
     Queue<Drafter> draftOrder = new ArrayDeque<Drafter>();
     draftOrder.add(new Drafter(new SimpleDraftStrategy(), "Ross"));
-    draftOrder.add(new Drafter(new OGDraftStrategy(2.0), "Joe"));
+    draftOrder.add(new Drafter(new OGDraftStrategy("QValues.csv", 0.95, .8, .2), "Joe"));
     draftOrder.add(new Drafter(new SimpleDraftStrategy(), "Caroline"));
     Draft draft = new Draft(draftOrder);
     draft.play(15);
--- OGDraftStrategy.java
+++ OGDraftStrategy.java
@@ -1,19 +1,43 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
 public class OGDraftStrategy extends AbstractDraftStrategy {
 
-    double index1;
+    Map<String, List<Integer>> qValues;
+    double Gamma;
+    double Alpha;
+    double Epsilon;
 
-    public OGDraftStrategy(double index1) {
-        this.index1 = index1;
+    public OGDraftStrategy(String filepath, double gamma, double alpha, double epsilon) {
+        this.qValues = this.PopulateQValues(filepath);
+        this.Gamma = gamma; //discount factor
+        this.Alpha = alpha; //learning rate
+        this.Epsilon = epsilon; //chance of random action
     } 
+
+    private Map<String, List<Integer>> PopulateQValues(String filepath) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'PopulateQValues'");
+    }
 
     @Override
     public Player draftPlayer(String me, List<Player> available, Queue<String> draftOrder, Map<String, List<Player>> drafted) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'draftPlayer'");
+    }
+
+    private String getState(List<Player> drafted) {
+        StringBuilder s = new StringBuilder();
+        for (Player p: drafted) {
+            s.append(p.getPosition().toString());
+        }
+        String original = s.toString();
+        char[] chars = original.toCharArray();
+        Arrays.sort(chars);
+        String sorted = new String(chars);
+        return sorted;
     }
     
 }
